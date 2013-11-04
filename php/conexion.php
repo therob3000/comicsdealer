bec71a5c02e10a48d41ed048be686382f1aec668
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2013-11-04 00:35:56 -0600

    Se cambio el archivo de conexion

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,21 @@
 <?php
 
 function conexion(){
+
+	$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"],1);
+
+    
+            
+    
+    
+
 	//Definimos los parametros de conexion, host, usuario, password
-	$con = mysql_connect("localhost","root","rufo666");
+	$con = mysql_connect($server, $username, $password);
 
 	//Si no se puede conectar manda el siguiente mensaje
 	if (!$con){
@@ -10,7 +23,7 @@ function conexion(){
 	}
 	//Seleccionamos la base de datos a usar
 	else{
-		mysql_select_db("comicsdealer", $con);
+		mysql_select_db($db);
 		return($con);
 	}
 }
