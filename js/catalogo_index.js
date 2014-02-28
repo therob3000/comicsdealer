65fd81cd1811133fa8318529b68adf7d1fe29a72
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-02-27 22:44:21 -0600

    paginador de catalogo

--- js/catalogo_index.js
+++ js/catalogo_index.js
@@ -14,16 +14,16 @@ function cargarComics(salto){
 		sigSalto = +sigSalto+3;
 	};
 	if(salto==0){
-				$("#anterior").html("<a href='#'>Anterior</a>");
-			}
-			else{
-				$("#anterior").html("<a href='./Catalogo.php?pagina="+(+salto-12)+"'>Anterior</a>");
-			}
-			if(+salto+12 >= total){
-				$("#siguiente").togglehide();
-			}
-			else{
-				$("#siguiente").html("<a href='./Catalogo.php?pagina="+(+salto+12)+"'>Siguiente</a>");
-			}
+		$("#anterior").togglehide();
+	}
+	else{
+		$("#anterior").html("<a href='./Catalogo.php?pagina="+(+salto-12)+"'>Anterior</a>");
+	}
+	if(+salto+12 >= total){
+		$("#siguiente").togglehide();
+	}
+	else{
+		$("#siguiente").html("<a href='./Catalogo.php?pagina="+(+salto+12)+"'>Siguiente</a>");
+	}
 }
 
