138a040a2f8bc40be31c1fa89c4ef7799c8988df
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-02-27 23:04:03 -0600

    Otro lol

--- js/catalogo_index.js
+++ js/catalogo_index.js
@@ -14,13 +14,14 @@ function cargarComics(salto){
 		sigSalto = +sigSalto+3;
 	};
 	if(salto==0){
-		$("#anterior").togglehide();
+		$("#anterior").hide();
 	}
 	else{
 		$("#anterior").html("<a href='./Catalogo.php?pagina="+(+salto-12)+"'>Anterior</a>");
 	}
 	if(+salto+12 > total){
-		$("#siguiente").togglehide();
+		alert(total);
+		$("#siguiente").hide();
 	}
 	else{
 		$("#siguiente").html("<a href='./Catalogo.php?pagina="+(+salto+12)+"'>Siguiente</a>");
