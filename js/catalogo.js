556a37b95e58fbbd67743e396433a016d1125e3c
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-03-08 13:19:25 -0600

    Modificacion a Catalago con el nuevo esquema de BD

--- js/catalogo.js
+++ js/catalogo.js
@@ -9,12 +9,12 @@ function cargarCatalogoComics (salto, rango) {
 			$.each(data.catalogo, function(i, val){
 				$.get("../html/layouts/catalogo_layout.html", function(data){
 					$("#catalogo_comics").append(data);
-					$("#catalogo_comics").find("#catalogo_comic").attr("id", val.cat_comic_id);
-					$("#"+val.cat_comic_id).find("#cat_imagen").attr("src", val.cat_comic_imagen_url);
-					$("#"+val.cat_comic_id).find("#cat_personaje").text(val.cat_comic_personaje)
-					$("#"+val.cat_comic_id).find("#cat_titulo").text(val.cat_comic_titulo+" #"+val.cat_comic_numero_ejemplar);
-					//$("#"+val.cat_comic_id).find("#cat_descripcion").text(val.cat_comic_descripcion);
-					$("#"+val.cat_comic_id).find("#cat_precio_venta").text("$"+val.cat_comic_precio_salida+" MXN");
+					$("#catalogo_comics").find("#catalogo_comic").attr("id", val.inventario_id);
+					$("#"+val.inventario_id).find("#cat_imagen").attr("src", val.cat_comic_imagen_url);
+					$("#"+val.inventario_id).find("#cat_personaje").text(val.cat_comic_personaje)
+					$("#"+val.inventario_id).find("#cat_titulo").text(val.cat_comic_titulo+" #"+val.cat_comic_numero_ejemplar);
+					$("#"+val.inventario_id).find("#cat_descripcion").text(val.cat_comic_descripcion);
+					$("#"+val.inventario_id).find("#cat_precio_venta").text("$"+val.inventario_precio_salida+" MXN");
 				});
 			});
 			
--- php/cargarCatalogo.php
+++ php/cargarCatalogo.php
@@ -9,28 +9,46 @@
 	$rango = $_GET["rango"];
 	$json = new stdClass();
 
-	$camposArray = array("cat_comic_id",
+	$camposArray = array("inventario_id",
 						"cat_comic_titulo",
 						"cat_comic_descripcion",
 						"cat_comic_personaje",
 						"cat_comic_numero_ejemplar",
-						"cat_comic_activo",
 						"cat_comic_imagen_url",
-						"cat_comic_precio_salida"
+						"inventario_precio_salida"
 	);
 
 	$catalogoArray = array();
 	$rowArray = array();
-	
 
-	$queryCatalogoComics = "SELECT 	cat_comic_id,
-									cat_comic_titulo,
-									cat_comic_descripcion,
-									(SELECT personaje_nombre FROM personajes WHERE personaje_id = cat_comic_personaje_id) as cat_comic_personaje,
-									cat_comic_numero_ejemplar,
-									cat_comic_activo,
-									cat_comic_imagen_url,
-									cat_comic_precio_salida FROM cat_comics LIMIT $salto, $rango";
+	$queryCatalogoComics = "SELECT 
+    INV.inventario_id,
+    CATALOGO.cat_comic_titulo,
+    CATALOGO.cat_comic_descripcion,
+    (SELECT 
+            personaje_nombre
+        FROM
+            personajes
+        WHERE
+            personaje_id = CATALOGO.cat_comic_personaje_id) as cat_comic_personaje,
+    CATALOGO.cat_comic_numero_ejemplar,
+	CATALOGO.cat_comic_imagen_url,
+    INV.inventario_precio_salida
+	FROM
+    cat_comics as CATALOGO
+        INNER JOIN
+    (SELECT 
+        inventario_id,
+		max(inventario_precio_entrada) as inventario_max_precio_entrada,
+		inventario_precio_salida,
+		inventario_cat_comic_unique_id,
+		inventario_existente
+    FROM
+        inventario
+    GROUP BY inventario_cat_comic_unique_id) AS INV ON INV.inventario_cat_comic_unique_id = CATALOGO.cat_comic_unique_id
+	WHERE
+    	CATALOGO.cat_comic_activo = 1 AND INV.inventario_existente = 1
+    LIMIT $salto, $rango";
 
 	$queryResultado = mysql_query($queryCatalogoComics);
 	$num = mysql_num_rows($queryResultado);
