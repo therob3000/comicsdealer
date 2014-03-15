13cc2c3cc06d596e901a756f9ba5a131a0dd0e5f
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-03-15 15:58:06 -0600

    lel111

--- js/catalogo.js
+++ js/catalogo.js
@@ -52,7 +52,7 @@ function cargarCatalogoComics2(salto, rango) {
 				$.get("../html/layouts/catalogo_layout.html", function(data2){
 					$("#catalogo_comics").append(data2);
 					$("#catalogo_comics").find("#catalogo_comic").attr("id", val.inventario_id);
-					$("#"+val.inventario_id).find('#boton_comprar').html("<a class='btn btn-success btn-comprar' href='/html/Catalogo.php' role='button'>Comprar >></a>");
+					$("#"+val.inventario_id).find('#boton_comprar').html("<button class='btn btn-success btn-comprar' href='/html/Catalogo.php' role='button'>Comprar</button>");
 					$("#"+val.inventario_id).find('#cat_detalle').attr('href', "/html/Detalle.php?comic_id="+val.inventario_id);
 					$("#"+val.inventario_id).find("#cat_imagen").attr("src", val.cat_comic_imagen_url);
 					$("#"+val.inventario_id).find("#cat_personaje").text(val.cat_comic_personaje)
@@ -68,22 +68,4 @@ function cargarCatalogoComics2(salto, rango) {
 		'json');
 	$.ajaxSetup({async:true});
 
-}
-
-
-
-function verificaSesion(){
-	$.ajaxSetup({async:false});
-	$.post("../php/verifica_sesion.php",
-		function(data){
-			verifica = data.ver_sesion.estado;
-			if(verifica == true){
-				$("#nav_bar").load("../html/layouts/navbar_login_layout.html");
-			}
-			else{
-				$("#nav_bar").load("../html/layouts/navbar_nologin_layout.html");
-			}
-		},
-		'json');
-	$.ajaxSetup({async:true});
 }
--- js/catalogo_index.js
+++ js/catalogo_index.js
@@ -1,18 +1,58 @@
 var total;
 
 $(document).ready(function(){
-	verificaSesion();
-	cargarComics(pagina);
-	botonComprar();
-	botonEliminar();
+	verificaSesion(pagina);
+	modalIniciarSesion();
 });
 
+function verificaSesion(pagina){
+	$.ajaxSetup({async:false});
+	$.post("../php/verifica_sesion.php",
+		function(data){
+			verifica = data.ver_sesion.estado;
+			if(verifica == true){
+				$("#nav_bar").load("../html/layouts/navbar_login_layout.html");
+				cargarComics(pagina);
+				botonComprar();
+				botonEliminar();
+			}
+			else{
+				$("#nav_bar").load("../html/layouts/navbar_nologin_layout.html");
+				cargarComicsNologin(pagina);
+				botonComprarNologin();
+			}
+		},
+		'json');
+	$.ajaxSetup({async:true});
+}
 
 function cargarComics(salto){
 	var sigSalto = salto;
 	for (var i = 0; i < 4; i++) {
 		$(".rows").append("<div class=row id=catalogo_comics></div>");
 		cargarCatalogoComics(sigSalto,3);
+		$("#catalogo_comics").attr("id", i);
+		sigSalto = +sigSalto+3;
+	};
+	if(salto==0){
+		$("#anterior").hide();
+	}
+	else{
+		$("#anterior").html("<a href='./Catalogo.php?pagina="+(+salto-12)+"'>Anterior</a>");
+	}
+	if(+salto+12 > total){
+		$("#siguiente").hide();
+	}
+	else{
+		$("#siguiente").html("<a href='./Catalogo.php?pagina="+(+salto+12)+"'>Siguiente</a>");
+	}
+}
+
+function cargarComicsNologin(salto){
+	var sigSalto = salto;
+	for (var i = 0; i < 4; i++) {
+		$(".rows").append("<div class=row id=catalogo_comics></div>");
+		cargarCatalogoComics2(sigSalto,3);
 		$("#catalogo_comics").attr("id", i);
 		sigSalto = +sigSalto+3;
 	};
@@ -47,5 +87,19 @@ function botonEliminar(){
 		$("#boton_comprar"+$(this).attr('id')).show();
 	});
 }
+
+function botonComprarNologin(){
+	$(".btn-comprar").on("click", function(){
+		$('#myModal').modal('show');
+	})
+}
+
+function modalIniciarSesion(){
+	$("#nav_bar").on("click", "#loginButton", function(e){
+		$('#myModal').modal('show');
+	});
+}
+
+
 
 
