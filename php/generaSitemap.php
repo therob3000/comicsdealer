448c5983cb64bc6c65e87a35ebcfda2ee6669e05
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-04-13 23:53:46 -0500

    Sitemap corregido

--- php/generaSitemap.php
+++ php/generaSitemap.php
@@ -5,14 +5,9 @@
 	ini_set('display_errors',1); 
 	error_reporting(E_ALL);
 
-	$queryDetalles = "SELECT inventario_id FROM inventario";
-	$queryResultado = mysql_query($queryDetalles);
+	
 
-
-	$num = mysql_num_rows($queryResultado);
-
-	if($num > 0){
-		echo "<?xml version='1.0' encoding='UTF-8'?>";
+	echo "<?xml version='1.0' encoding='UTF-8'?>";
 		echo "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9' xmlns:image='http://www.google.com/schemas/sitemap-image/1.1'>";
 		echo "<url>
 				<loc>http://www.comicsdealer.com/html/ArticulosIndex.php</loc>
@@ -39,11 +34,32 @@
 			  	<changefreq>never</changefreq>
 			  </url>
 			  ";
+
+	$queryDetalles = "SELECT inventario_id FROM inventario";
+	$queryResultado = mysql_query($queryDetalles);
+	$num = mysql_num_rows($queryResultado);
+	if($num > 0){
+		
 		for ($i=0; $i < $num ; $i++) { 
 			echo "<url>
-					<loc>http://www.comicsdealer.com/html/Detalle.php?comid_id=".mysql_result($queryResultado, $i, "inventario_id")."</loc>
+					<loc>http://www.comicsdealer.com/html/Detalle.php?comic_id=".mysql_result($queryResultado, $i, "inventario_id")."</loc>
 					<changefreq>weekly</changefreq>
 				  </url>";
 		}
-		echo "</urlset>";
+
+		
 	}
+
+	$queryArticulos = "Select articulo_id from articulos";
+	$queryResultado2 = mysql_query($queryArticulos);
+	$num = mysql_num_rows($queryResultado2);
+	if($num > 0){
+		for ($i=0; $i < $num ; $i++) { 
+			echo "<url>
+					<loc>http://www.comicsdealer.com/html/Articulos.php?articulo_id=".mysql_result($queryResultado2, $i, "articulo_id")."</loc>
+					<changefreq>weekly</changefreq>
+				  </url>";
+		}
+	}
+
+	echo "</urlset>";
