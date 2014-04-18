d2bb9d6d5313c1efa24b278bad434ca80a616b0c
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-04-17 19:11:11 -0500

    lel4

--- html/faceLogin.php
+++ html/faceLogin.php
@@ -65,6 +65,6 @@
   Below we include the Login Button social plugin. This button uses the JavaScript SDK to
   present a graphical Login button that triggers the FB.login() function when clicked. -->
 
-<fb:login-button show-faces="true" width="200" max-rows="1" data-scope:"email"></fb:login-button>
+<fb:login-button show-faces="true" width="200" max-rows="1" data-scope="email"></fb:login-button>
 </body>
 </html>
