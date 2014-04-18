880da52440105cf6d0a13aed2cd9b2d6bedfeaf7
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-04-17 21:29:16 -0500

    lel8

--- index.php
+++ index.php
@@ -15,6 +15,33 @@
   <link rel="shortcut icon" href="img/ComicDico-01.png">
   <link href="bootstrap/css/navbar.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="bootstrap/css/comicsD.css">
+  <script>
+        window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '655150577891800',
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true  // parse XFBML
+        });
+        
+    };
+    (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
+
+  // Here we run a very simple test of the Graph API after login is successful. 
+  // This testAPI() function is only called in those cases. 
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+  }
+  </script>
   <script src="/bootstrap/assets/js/jquery.js"></script>
   <script src="/bootstrap/js/bootstrap.min.js"></script>
   <script src="/js/catalogo.js"></script>
@@ -50,33 +77,7 @@
     </head>
     <body>
       <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '655150577891800',
-          status     : true, // check login status
-          cookie     : true, // enable cookies to allow the server to access the session
-          xfbml      : true  // parse XFBML
-        });
-        
-    };
-    (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
-  }(document));
-
-  // Here we run a very simple test of the Graph API after login is successful. 
-  // This testAPI() function is only called in those cases. 
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  }
-  </script>
+      
 
       <!--<script>(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
