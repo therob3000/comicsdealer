37de4e310a3d1dc21d6125c17fe67b52775c8704
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-04-18 07:08:07 -0500

    gyguygu

--- js/registro.js
+++ js/registro.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
 			cadena,
 			function(data){
 				existe_email = data.correo;
-				/alert(existe_email);
+				//alert(existe_email);
 				if(existe_email == true){
 					//alert(existe_email);
 					$('#formemail').append('<div class="alert alert-danger"><strong>Este correo ya está registrado</strong> vuelve a intentarlo.</div>');
