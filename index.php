ba36a6068459444cafa6c895d0dc257753b2c70a
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-04-17 21:56:28 -0500

    lel11

--- index.php
+++ index.php
@@ -16,7 +16,22 @@
   <link href="bootstrap/css/navbar.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="bootstrap/css/comicsD.css">
   <script>
+        window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '655150577891800',
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true  // parse XFBML
+        });
         
+    };
+    (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
   </script>
   <script src="/bootstrap/assets/js/jquery.js"></script>
   <script src="/bootstrap/js/bootstrap.min.js"></script>
--- js/login.js
+++ js/login.js
@@ -1,33 +1,4 @@
-var respuestaFacebook;
 $(document).ready(function(){
-	window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '655150577891800',
-          status     : true, // check login status
-          cookie     : true, // enable cookies to allow the server to access the session
-          xfbml      : true  // parse XFBML
-        });
-        
-    };
-    (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
-  }(document));
-
-  // Here we run a very simple test of the Graph API after login is successful. 
-  // This testAPI() function is only called in those cases. 
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  }
-	FB.Event.subscribe('auth.authResponseChange', function(response) {
-	    	respuestaFacebook = response;
-	    });
 	login();
 	cerrar_sesion();
 	cargar_info();
@@ -98,24 +69,17 @@ function cargar_info() {
 }
 
 function registroFacebook(){
-	$("#registro_facebook").click(function(e){
-		/*alert("lel");
-          if (respuestaFacebook.status === 'connected') {
-             window.location.href = "/html/Catalogo.php";
-          } else if (respuestaFacebook.status === 'not_authorized') {
-            FB.login();
-        } else {
-          FB.login();
-        }*/
-	    
-	    if (respuestaFacebook.status === 'connected') {
-             window.location.href = "/html/Catalogo.php";
-          } else if (respuestaFacebook.status === 'not_authorized') {
-            FB.login();
-        } else {
-          FB.login();
-        }
-	     
+	FB.Event.subscribe('auth.authResponseChange', function(response) {
+		if (response.status === 'connected') {
+			
+			alert("conectado");
+		} else if (response.status === 'not_authorized') {
+			
+			FB.login();
+		} else {
+			
+			FB.login();
+		}
 	});
-	
+
 }
