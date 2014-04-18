ed809af13390edd7000e2e383ec7964fa9fc4c8c
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-04-17 21:35:49 -0500

    lel9

--- index.php
+++ index.php
@@ -16,31 +16,7 @@
   <link href="bootstrap/css/navbar.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="bootstrap/css/comicsD.css">
   <script>
-        window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '655150577891800',
-          status     : true, // check login status
-          cookie     : true, // enable cookies to allow the server to access the session
-          xfbml      : true  // parse XFBML
-        });
         
-    };
-    (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
-  }(document));
-
-  // Here we run a very simple test of the Graph API after login is successful. 
-  // This testAPI() function is only called in those cases. 
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  }
   </script>
   <script src="/bootstrap/assets/js/jquery.js"></script>
   <script src="/bootstrap/js/bootstrap.min.js"></script>
