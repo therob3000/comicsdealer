c7773863af1c6907e5cef378cfea45b13067f21e
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-04-17 23:36:46 -0500

    lel16

--- index.php
+++ index.php
@@ -70,7 +70,12 @@ window.fbAsyncInit = function() {
       // The response object is returned with a status field that lets the app know the current
       // login status of the person. In this case, we're handling the situation where they 
       // have logged in to the app.
+      FB.api('/me', function(response) {
+        alert('Good to see you, ' + response.name + '.');
+      });
       window.location.href = "/html/Catalogo.php";
+      
+
     } else if (response.status === 'not_authorized') {
       // In this case, the person is logged into Facebook, but not into the app, so we call
       // FB.login() to prompt them to do so. 
