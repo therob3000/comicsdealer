b61a8b8c09e65265fd0508dde6aa264025443b3e
yunrock <yunuenvladimirsanchezgarrido@MacBook-Pro-de-Yunuen.local>
2014-04-24 22:47:27 -0500

    lol extremo

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
 
   <style>
     .container {
-      background: url(img/bg1.jpg) no-repeat center center fixed;
+      background: url(img/AVXM31.jpg) no-repeat center center fixed;
       background-size: cover;
     }
   </style>
