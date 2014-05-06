b59b43e5e2611f082e2ef141a09b8dbbc15aeb27
yunrock <yunuenvladimirsanchezgarrido@mbp-de-yunuen.lan>
2014-05-05 23:22:04 -0500

    ya está listo lo de los cómics mini

--- js/catalogo.js
+++ js/catalogo.js
@@ -13,6 +13,12 @@ function cargarCarouselComics(salto, rango, capa) {
             total = data.total;
             $.each(data.catalogo, function(i, val) {
               $.get(capa, function(data2) {
+                idioma = "";
+                if (val.cat_comic_idioma == "ing") {
+                  idioma = "Inglés";
+                } else if (val.cat_comic_idioma == "esp") {
+                  idioma ="Español";
+                }
 
                 $("#carousel_comics").append(data2);
                 $("#carousel_comics").find("#catalogo_comic").attr("id", val.inventario_id);
@@ -20,15 +26,12 @@ function cargarCarouselComics(salto, rango, capa) {
                 //$("#"+val.inventario_id).find('#boton_comprar').html("<button class='btn btn-success btn-comprar btn-small' role='button'>$"+val.inventario_precio_salida+"<small>MXN</small></button>");
                 $("#" + val.inventario_id).find('#cat_detalle').attr('href', "/html/Detalle.php?comic_id=" + val.inventario_id);
                 $("#" + val.inventario_id).find("#cat_imagen").attr("src", val.cat_comic_imagen_url);
-                $("#" + val.inventario_id).find("#cat_personaje").text(val.cat_comic_personaje);
-                $("#" + val.inventario_id).find("#cat_titulo").text(val.cat_comic_titulo + " #" + val.cat_comic_numero_ejemplar);
+                $("#" + val.inventario_id).find("#cat_personaje").html(val.cat_comic_personaje + "<br><titulo>" + val.cat_comic_titulo + " #" + val.cat_comic_numero_ejemplar + "</titulo>" + 
+                        "<br><idioma>" + idioma + "</idioma>" + "<br><precio>$" + val.inventario_precio_salida + "<small> MXN</small></precio>");
+                //$("#" + val.inventario_id).find("#cat_titulo").html(val.cat_comic_titulo + " #" + val.cat_comic_numero_ejemplar);
+
                 
-                if (val.cat_comic_idioma == "ing") {
-                  $("#" + val.inventario_id).find("#cat_idioma").text("Inglés");
-                } else if (val.cat_comic_idioma == "esp") {
-                  $("#" + val.inventario_id).find("#cat_idioma").text("Español");
-                }
-                $("#"+val.inventario_id).find("#cat_precio_venta").text("$"+val.inventario_precio_salida);
+                $("#" + val.inventario_id).find("#cat_precio_venta").text("$" + val.inventario_precio_salida);
 
                 //VIEJO!!!!!!!--------
 //                $("#carousel_comics").append(data2);
